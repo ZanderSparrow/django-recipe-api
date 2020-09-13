8b32ec8a08c9049ebdbc02d85f3bc8ea9d2effe9
Alex Sparrow <alexsilverhawk@gmail.com>
2020-09-12 18:31:47 -0700

    Fix bug, add migrations to gitignore

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model the supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
-    name = models.Charfield(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
