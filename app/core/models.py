dfad0e33700cb2c2616bfedd957c6d8d988befc5
Alex Sparrow <alexsilverhawk@gmail.com>
2020-09-15 12:34:23 -0700

    Add custom user model, create_superuser function, and tests

--- app/core/models.py
+++ app/core/models.py
@@ -6,8 +6,19 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create a user with email and password"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create user with is_staff and is_superuser set to true"""
+        user = self.create_user(email, password, **extra_fields)
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,3 +14,27 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@GMAIL.com'
+        user = get_user_model().objects.create_user(email, password='pass1234')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+        
