d4b505f74d5a743bf8f71d48219333f4497f681e
Alex Sparrow <alexsilverhawk@gmail.com>
2020-09-30 17:48:58 -0700

    Add list ingredient endpoint and factor our common viewset elements

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,24 +2,34 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Ingredient, Tag
 
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manage tags in the database"""
+class BaseViewSet(viewsets.GenericViewSet,
+                  mixins.ListModelMixin,
+                  mixins.CreateModelMixin):
+    """Common management operations for objects in database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
         """Only return tags belinging to authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
+
+class TagViewSet(BaseViewSet):
+    """Manage tags in the database"""
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
     def perform_create(self, serializer):
         """Create a new tag and associate authenticated user"""
         serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(BaseViewSet):
+    """Manage ingredients in database"""
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
